27b45dd824384189a319264933d23fb248977d20
v_ljjlwu <v_ljjlwu@tencent.com>
2022-08-09 02:21:28 +0800

    feat: 注册页面实现表单验证

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -11,40 +11,54 @@
     </div>
     <div class="main">
       <div class="desc"><h2>注册</h2></div>
-      <div>
-        <el-input v-model="accountName" placeholder="用户名"></el-input>
-      </div>
-      <div>
-        <el-input
-          v-model="password"
-          placeholder="请输入密码"
-          type="password"
-          showpassword
-        ></el-input>
-      </div>
-      <div>
-        <el-input
-          v-model="passwordAgain"
-          placeholder="请输入确认密码"
-          type="password"
-          showpassword
-        ></el-input>
-      </div>
-      <div>
-        <el-input v-model="email" placeholder="请输入邮箱"></el-input>
-      </div>
-      <div id="verify-code">
-        <el-input
-          class="code-input"
-          v-model="verifyCode"
-          placeholder="验证码"
-        ></el-input>
-        <el-button class="code-button">获取验证码</el-button>
-      </div>
-      <div class="submit">
-        <el-button type="primary">注册</el-button>
-        <el-link type="primary">使用已有账号登录</el-link>
-      </div>
+      <el-form :model="formData" :rules="rules">
+        <el-form-item prop="accountName">
+          <el-input
+            v-model="formData.accountName"
+            placeholder="用户名"
+            prop="accountName"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            v-model="formData.password"
+            placeholder="请输入密码"
+            type="password"
+            showpassword
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="passwordAgain">
+          <el-input
+            v-model="formData.passwordAgain"
+            placeholder="请输入确认密码"
+            type="password"
+            showpassword
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="email">
+          <el-input
+            v-model="formData.email"
+            placeholder="请输入邮箱"
+          ></el-input>
+        </el-form-item>
+        <el-form-item id="verify-code" prop="emailCode">
+          <el-input
+            class="code-input"
+            v-model="formData.emailCode"
+            placeholder="验证码"
+          ></el-input>
+          <el-button
+            class="code-button"
+            @click="clickLimit()"
+            :disabled="lockBtn"
+            >{{ codeBtn }}</el-button
+          >
+        </el-form-item>
+        <el-form-item class="submit">
+          <el-button type="primary" @click="register">注册</el-button>
+          <el-link type="primary" href="/login">使用已有账号登录</el-link>
+        </el-form-item>
+      </el-form>
     </div>
     <div class="footer">
       <div class="links">
@@ -60,7 +74,86 @@
 </template>
 
 <script>
-export default {};
+import { reactive, ref } from "vue";
+
+export default {
+  setup() {
+    const formData = reactive({
+      accountName: "",
+      password: "",
+      passwordAgain: "",
+      email: "",
+      emailCode: "",
+    });
+    let codeBtn = ref("获取验证码");
+    const rules = {
+      accountName: {
+        required: true,
+        message: "只允许字母和数字的组合",
+        trigger: "blur",
+        pattern: /^[0-9a-z]+$/i,
+      },
+      password: {
+        required: true,
+        message: "必须是大于6个字符的字母和数字的组合",
+        trigger: "blur",
+        validator: (rule, value) => {
+          return (
+            value.length > 6 && /[a-z]+/i.test(value) && /[0-9]+/.test(value)
+          );
+        },
+      },
+      passwordAgain: {
+        required: true,
+        message: "两次输入的密码不相同",
+        trigger: "blur",
+        validator: (rule, value) => value === formData.password,
+      },
+      email: {
+        required: true,
+        message: "邮箱不合法",
+        trigger: "blur",
+        type: "email",
+      },
+      emailCode: {
+        required: true,
+        message: "请输6位入数字验证码",
+        trigger: "blur",
+        pattern: /^[0-9]{6,}$/,
+      },
+    };
+
+    let lockBtn = ref(false);
+    const clickLimit = () => {
+      // 倒计时
+      if (!lockBtn.value) {
+        lockBtn.value = !lockBtn.value;
+        let seconds = 60;
+
+        setInterval(() => {
+          if (seconds > 0) {
+            codeBtn.value = seconds;
+            seconds -= 1;
+          } else {
+            clearInterval;
+            lockBtn.value = !lockBtn.value;
+            codeBtn.value = "获取验证码";
+          }
+        }, 1000);
+      }
+    };
+    const register = () => {
+      //TODO: 将表单数据提交到后台，如果成功则跳转到登录页，失败则提示
+    };
+    return {
+      formData,
+      rules,
+      codeBtn,
+      clickLimit,
+      register,
+    };
+  },
+};
 </script>
 
 <style lang="scss">
