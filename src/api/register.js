eedea821fe53f4650b010fc6d5b12b8ee5be25be
v_ljjlwu <v_ljjlwu@tencent.com>
2022-08-15 17:12:05 +0800

    fix: 解决axios get方法封装错误问题

--- src/api/register.js
+++ src/api/register.js
@@ -7,15 +7,15 @@ const api = {
 
 // 获取邮箱验证码
 export function getEmailCode(param) {
-  return request(api.GetEmailCode,{
+  return request(api.GetEmailCode, {
     method: "get",
-    data: param,
+    params: param,
   });
 }
 
 // 提交注册表单信息
 export function submitRegisterForm(param) {
-  return request(api.SubmitRegisterForm,{
+  return request(api.SubmitRegisterForm, {
     method: "post",
     data: param,
   });
