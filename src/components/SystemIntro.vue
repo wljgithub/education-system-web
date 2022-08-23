f7d0be7737a4a2c3851489a04f8b7dcaf9d6ece5
jackwu <wulingjian123@gmail.com>
2022-08-23 15:12:31 +0800

    refactor: 封装登录页header

---
+++ src/components/SystemIntro.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="header">
+    <div class="title">
+      <img src="../assets/login-logo.svg" alt="logo" />
+      <h1>教务管理系统</h1>
+    </div>
+    <div class="desc">
+      <p>一个专注于为师生服务的管理系统</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SystemIntro",
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  margin-top: 80px;
+  width: 350px;
+  height: 170px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  .title {
+    width: 100%;
+    height: 115px;
+    display: flex;
+    justify-content: space-between;
+  }
+  .desc {
+    margin-top: 30px;
+  }
+}
+</style>
